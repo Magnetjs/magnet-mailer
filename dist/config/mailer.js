8c2e3a4d2b50db0c9414e0da9eba5a9266071d74
Kieve Chua <kievechua@gmail.com>
2017-02-25 15:50:25 +0800

    Upgrade core

--- dist/config/mailer.js
+++ dist/config/mailer.js
@@ -1,33 +1,7 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
 // https://nodemailer.com/about/
 // import mandrillTransport from 'nodemailer-mandrill-transport'
 // import htmlToText from 'nodemailer-html-to-text'
-
-exports.default = {
-  // plugins: {
-  //   compile: htmlToText(),
-  //   stream: 'signer(options)'
-  // }
-
-  // // By SMTP
-  // // https://github.com/nodemailer/nodemailer#set-up-smtp
-  // transport: {
-  //   host: 'smtp.gmail.com',
-  //   port: 465,
-  //   secure: true, // use SSL
-  //   auth: {
-  //     user: 'user@gmail.com',
-  //     pass: 'pass'
-  //   }
-  // },
-  //
-  // transport: mandrillTransport({
-  //   auth: {
-  //     apiKey: 'apiKey'
-  //   }
-  // })
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {};
+//# sourceMappingURL=mailer.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import Base from 'magnet-core/base'
-import nodemailer from 'nodemailer'
+import { Module } from 'magnet-core/module'
+import * as nodemailer from 'nodemailer'
+
 import defaultConfig from './config/mailer'
 
-export default class Mailer extends Base {
+export default class Mailer extends Module {
   async setup () {
-    let config = Object.assign(defaultConfig, this.config.mailer, this.options)
+    const config = this.prepareConfig('mailer', defaultConfig)
 
     // create reusable transporter method (opens pool of SMTP connections)
     this.app.nodemailer = nodemailer.createTransport(config.transport)
